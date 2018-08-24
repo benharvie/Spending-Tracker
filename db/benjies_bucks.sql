2ecd59216354bd253fa58154469642414d4fdbc6
Ben Harvie <benharvie94@gmail.com>
2018-08-24 12:26:17 +0100

    DB created

---
+++ db/benjies_bucks.sql
@@ -0,0 +1,21 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+DROP TABLE tags;
+
+
+CREATE TABLE merchants (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE tags (
+  id SERIAL8 PRIMARY KEY,
+  category VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+  id SERIAL8 PRIMARY KEY,
+  description VARCHAR(255),
+  merchant_id INT8 REFERENCES merchants(id),
+  tag_id INT8 REFERENCES tags(id)
+);
