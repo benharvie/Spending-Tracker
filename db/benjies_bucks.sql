3b2d8a3d3e381bd5c5e2025b7ad18459efe22db9
Ben Harvie <benharvie94@gmail.com>
2018-08-24 12:48:21 +0100

    DB refactored

--- db/benjies_bucks.sql
+++ db/benjies_bucks.sql
@@ -16,6 +16,7 @@ CREATE TABLE tags (
 CREATE TABLE transactions (
   id SERIAL8 PRIMARY KEY,
   amount REAL,
+  transaction_date DATE,
   description VARCHAR(255),
   merchant_id INT8 REFERENCES merchants(id),
   tag_id INT8 REFERENCES tags(id)
