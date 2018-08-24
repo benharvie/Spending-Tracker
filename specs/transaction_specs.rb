8e08729ea4025caa0acafcaa62e60c009fb3da3b
Ben Harvie <benharvie94@gmail.com>
2018-08-24 13:45:50 +0100

    test_transaction_has_amoint passes

--- specs/transaction_specs.rb
+++ specs/transaction_specs.rb
@@ -4,10 +4,14 @@ require_relative '../models/transaction.rb'
 
 class TestTransaction < MiniTest::Test
   def setup
-    @transaction1 = Transaction.new('description' => 'Bought some shoes', 'amount' => 50.00, 'date' => '24/08/18')
+    @transaction1 = Transaction.new('description' => 'Bought some shoes', 'amount' => 50.00, 'date' => '24/08/2018')
   end
 
   def test_transaction_has_description
     assert_equal('Bought some shoes', @transaction1.description)
+  end
+
+  def test_transaction_has_amount
+    assert_equal(50.00, @transaction1.amount)
   end
 end
