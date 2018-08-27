6f3ad656764cc1af7ecddbbaaa1095eabf606981
Ben <benharvie94@gmail.com>
2018-08-27 10:11:44 +0100

    update function working

--- models/transaction.rb
+++ models/transaction.rb
@@ -1,10 +1,30 @@
 require '../db/sql_runner.rb'
 
 class Transaction
-  attr_accessor :description, :amount, :date
+  attr_accessor :description, :amount, :date, :id
   def initialize(options)
     @description = options['description']
     @amount = options['amount']
     @date = options['date']
   end
+
+  def save
+    sql = "INSERT INTO transactions
+    (description, amount, transaction_date)
+    VALUES
+    ($1, $2, $3)
+    RETURNING id"
+    values = [@description, @amount, @date]
+    @id = SqlRunner.run(sql, values).first['id'].to_i
+  end
+
+  def update
+    sql = "UPDATE transactions
+          SET (description, amount, transaction_date)
+          = ($1, $2, $3)
+          WHERE id = $4"
+    values = [@description, @amount, @date, @id]
+    SqlRunner.run(sql, values)
+  end
+  
 end
