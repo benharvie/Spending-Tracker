364ca79f0b3661c6556961248eeecdc9be1c7a4b
Ben <benharvie94@gmail.com>
2018-08-28 10:44:37 +0100

    New tag is working

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -11,3 +11,13 @@ get '/tags' do
   @tag_list = Tag.all
   erb :"tags/index"
 end
+
+get '/tags/new' do
+  erb :"tags/new"
+end
+
+post '/tags' do
+  new_tag = Tag.new({ 'category' => params['category'] })
+  new_tag.save
+  redirect to '/tags'
+end
